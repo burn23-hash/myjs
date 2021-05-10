20a67528c0af1c0b02c6c4f70370782606bdea92
burn23-hash <optimissrithika23@gmail.com>
2021-05-10 21:26:46 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+    //let firstName = 'mein';
+    //let lastName;
+    //lastName = firstName;
+    //alert(lastName);
+    //[1, 2].forEach(alert);
+    document.getElementById("button_yes").onclick = function() 
+    {
+
+        document.getElementById("Confrim").innerHTML = "purchase is confimed";
+       // document.getElementById("button_no").innerHTML = "purchase is cancelled";
+        document.getElementById("button_yes").style.display="none";
+    }
+   /* document.getElementById("button_no").onclick = function() 
+    {
+
+        document.getElementById("Confrim").innerHTML = "purchase is cancelled";
+       // document.getElementById("button_no").innerHTML = "purchase is cancelled";
+        document.getElementById("button_no").style.display="none";
+    }*/
+
+    
